0ee27b0d1fc931433b9c9cd11f236a2bc3d14efb
unohayato <mamidori1p1p@gmail.com>
2022-08-28 22:07:08 +0900

    set

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/unohayato/Documents/GitHub/django_response/myvenv/bin/python3.9
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
