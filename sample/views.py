881de3f8d79d1b2306a9e85cdc62246cef6d38cf
unohayato <mamidori1p1p@gmail.com>
2022-08-28 22:13:44 +0900

    simple httpresponse

--- sample/views.py
+++ sample/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.views.generic import View
+from django.http import HttpResponse
 
-# Create your views here.
+class IndexView(View):
+  def get(self, request):
+    return HttpResponse('Hello')
